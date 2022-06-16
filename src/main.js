2674cedd44883a56eefb277708749fc5ed45c51f
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-06-16 20:03:13 +0400

    next App test

--- src/main.js
+++ src/main.js
@@ -1,14 +1,5 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./theme.css";
-import TheHeader from "./components/TheHeader.vue";
-import TheFooter from "./components/TheFooter.vue";
-import TheBody from "./components/TheBody.vue";
+import { createApp } from 'vue'
+import App from './App.vue'
+import './theme.css'
 
-const app = createApp(App);
-
-app.component("the-header", TheHeader);
-app.component("the-body", TheBody);
-app.component("the-footer", TheFooter);
-
-app.mount("#app");
+createApp(App).mount('#app')
