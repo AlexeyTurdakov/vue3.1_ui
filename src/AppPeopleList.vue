0d3c6110ea0e3a54d5fba0ca30ad41b8396fe15e
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-07-01 12:32:37 +0400

    AppAlert + db

--- src/AppPeopleList.vue
+++ src/AppPeopleList.vue
@@ -1,12 +1,28 @@
 <template>
-  <div></div>
+  <div v-if="people.length !== 0">
+    <div class="card line" v-for="person in people" :key="person.id">
+      <h3>{{ person.firstName }}</h3>
+      <button class="btn danger" @click="$emit('remove', person.id)">
+        Delete
+      </button>
+    </div>
+  </div>
+  <div v-else class="card center">
+    <button class="btn" @click="$emit('load')">Download people list</button>
+  </div>
 </template>
 
 <script>
 export default {
+  emits: ["load", "remove"],
   props: ["people"],
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.line {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
 </style>
