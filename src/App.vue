fda2eae70e9c37c45f2a70b4e18b99724f7cb81a
Alexey Turdakov <alexeyturdakov@gmail.con>
2022-07-26 18:09:48 +0400

    composition api finish

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,20 @@
 <template>
   <the-navbar></the-navbar>
   <div class="container with-nav">
-    <div class="card">
-      <h1>Про Vuex</h1>
-      <h2>Счетчик {{ counter }} {{ doubleCounter }}</h2>
-      <button class="btn" @click="add">btn++</button>
-      <button class="btn" @click="add10">btnAsync10</button>
-    </div>
+    <router-view />
   </div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
-import TheNavbar from "./TheNavbar";
+import TheNavbar from './components/TheNavbar'
+
 export default {
-  components: { TheNavbar },
-  computed: { ...mapGetters(["counter", "doubleCounter"]) },
-  methods: {
-    add() {
-      this.$store.commit({
-        type: "add",
-        value: { a: 5, b: 10 },
-      });
-    },
-    add10() {
-      this.$store.dispatch("addincassync");
-    },
-  },
-};
+  components: {
+    TheNavbar
+  }
+}
 </script>
+
+<style>
+
+</style>
